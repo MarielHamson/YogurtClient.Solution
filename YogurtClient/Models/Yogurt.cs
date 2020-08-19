57966f7bbaad5cf0c2d226b17a1ee1743deb1bbc
Allison Sadin <aesadin@gmail.com>
2020-08-19 11:10:04 -0700

    add all files to directory and merge into root directory called Yogurts
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    co-authored-by: Megan Hepner<megan.hepner@gmail.com>

---
+++ YogurtClient/Models/Yogurt.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace YogurtClient.Models
+{
+  public class Yogurt
+  {
+    public int YogurtId { get; set; }
+    [Required]
+    public string Brand { get; set; }
+    [Required]
+    public string Flavor { get; set; }
+    [Required]
+    public bool Blended { get; set; }
+    [Required]
+    public string Type { get; set; }
+    [Range(0, 500, ErrorMessage = "Must be between 1 and 500.")]
+    public int Protein { get; set; }
+    [Range(0, 500, ErrorMessage = "Must be between 1 and 500.")]
+    public int Sugar { get; set; }
+    [Range(0, 500, ErrorMessage = "Must be between 1 and 500.")]
+    public int Fat { get; set; }
+    [Range(0, 500, ErrorMessage = "Must be between 1 and 500.")]
+    public int Carbs { get; set; }
+    [Range(0, 1000, ErrorMessage = "Must be between 1 and 1000.")]
+    public int Calories { get; set; }
+
+    public static List<Yogurt> GetYogurts()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Yogurt> yogurtList = JsonConvert.DeserializeObject<List<Yogurt>>(jsonResponse.ToString());
+
+      return yogurtList;
+    }
+  }
+}
